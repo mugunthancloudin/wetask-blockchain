14a938206b77cb54953901efe01591d7dbf17cbf
mugunthancloudin <mugunthan@cloudinlabs.com>
2024-02-20 16:51:50 +0530

    campain module added

--- src/pages/campaign.js
+++ src/pages/campaign.js
@@ -2,12 +2,15 @@ import React from 'react';
 import MyNavbar from '../components/navbar & footer/navbar/navbar'
 import UnderConstruction from '../components/underContruction/underConstruction'
 import Footer from '../components/navbar & footer/footer/footer'
+import CampaignHome from '../components/campaign/campaignHome';
 
 export default function Campaign() {
   return (
     <>
       <MyNavbar/>
-      <UnderConstruction/>
+      <CampaignHome/>
+      {/* <UnderConstruction/> */}
+      
       <Footer/>
     </>
   )
