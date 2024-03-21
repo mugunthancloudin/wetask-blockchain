9b821189b0e5ae491bf2e73b117bd2e2ba2c58fe
mugunthancloudin <mugunthan@cloudinlabs.com>
2024-03-21 14:29:09 +0530

    event campaign module added

--- src/pages/event.js
+++ src/pages/event.js
@@ -7,7 +7,8 @@ export default function Event() {
   return (
     <>
       <MyNavbar/>
-      <EventCampaign />
+      <UnderConstruction/>
+      {/* <EventCampaign /> */}
       <Footer/>
     </>
   )
