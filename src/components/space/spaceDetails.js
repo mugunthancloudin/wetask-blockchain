1050372c8fde96c140c8ab402dd2bf92f92d1e7e
mugunthancloudin <mugunthan@cloudinlabs.com>
2024-04-30 10:02:05 +0530

    space details updated

--- src/components/space/spaceDetails.js
+++ src/components/space/spaceDetails.js
@@ -3,28 +3,29 @@ import "./spaceMain.css";
 import MyNavbar from "../navbar & footer/navbar/navbar";
 import Footer from "../navbar & footer/footer/footer";
 import { useLocation } from "react-router";
+import { useReadCampaign } from "../../services/blockchain";
+
 
 export default function SpaceDetails() {
   const location = useLocation();
   const { accumulatedData } = location?.state || {};
-  console.log(accumulatedData);
-
   const [eventDetail, setEventDetail] = useState(null);
   const spaceIndex = parseInt(window.location.pathname.split("/")[2], 10);
-  console.log(spaceIndex);
+
+const camaignId = 
+
+//   const { data, isSuccess } =  useReadCampaign(campaignId);
+
+
 
   useEffect(() => {
-    if (
-      accumulatedData &&
-      spaceIndex >= 0 &&
-      spaceIndex < accumulatedData.length
-    ) {
+    if (accumulatedData && spaceIndex >= 0 && spaceIndex < accumulatedData.length) {
       const selectedSpace = accumulatedData[spaceIndex];
       setEventDetail(selectedSpace);
     }
   }, [spaceIndex, accumulatedData]);
 
-  console.log(eventDetail);
+  
 
   return (
     <>
@@ -32,36 +33,46 @@ export default function SpaceDetails() {
       <div className="container-fluid text-white campaignMainBg">
         <div className="container">
           <div className="row">
-            <div className="col-lg-6 ">
-              <h1>hs</h1>
+            <div className="col-lg-6">
+              {eventDetail && <h3 className="pt-3">{eventDetail.name}</h3>}
             </div>
-            <div className="col-lg-6  d-flex justify-content-end">
-              <button>Follow</button>
-              <button className="ms-5">Share</button>
+            <div className="col-lg-6 d-flex justify-content-end">
+              <button className="spaceDetailBtn">Follow</button>
+              <button className="ms-5 spaceDetailBtn">Share</button>
             </div>
           </div>
         </div>
 
         <div className="row mt-5 p-5">
           <div className="col-lg-6">
-            <h3>{eventDetail.name}</h3>
-            <p>
-              The first video game that merges console gaming with blockchain
-              technology, developed by Galaxy Games. Featured on PlayStation,
-              Xbox, IGN, and Steam platforms, and incubated by Seedify Fund.
-              Winner of the Epic Mega Grant.
-            </p>
+            {eventDetail && (
+              <>
+                <h3>{eventDetail.name}</h3>
+                <p>   z``
+                  The first video game that merges console gaming with blockchain
+                  technology, developed by Galaxy Games. Featured on PlayStation,
+                  Xbox, IGN, and Steam platforms, and incubated by Seedify Fund.
+                  Winner of the Epic Mega Grant.
+                </p>
+              </>
+            )}
           </div>
-          <div className="col-lg-6 d-flex justify-content-between  text-center pt-5">
-            <div className="col-lg-4">
-            {eventDetail.campaigns.length}<br></br>campaigns
-            </div>
-            <div className="col-lg-4">
-            {eventDetail.events.length}<br></br>events
-            </div>
-            <div className="col-lg-4">
-              20<br></br>Partispants
-            </div>
+          <div className="col-lg-6 d-flex justify-content-be tween text-center pt-5">
+            {eventDetail && (
+              <>
+                <div className="col-lg-4">
+                  {eventDetail.campaigns.length}
+                  <br />campaigns
+                </div>
+                <div className="col-lg-4">
+                  {eventDetail.events.length}
+                  <br />events
+                </div>
+                <div className="col-lg-4">
+                  20<br />Participants
+                </div>
+              </>
+            )}
           </div>
 
           <div className="row mt-4">
